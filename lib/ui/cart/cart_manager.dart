7defec9172d8f47917ef8a0a3e8b9f0ad891c4d3
Vinh Nguyen <vinh466@gmail.com>
2022-09-20 19:32:53 +0700

    Xay dung trang gio hang

---
+++ lib/ui/cart/cart_manager.dart
@@ -0,0 +1,39 @@
+import 'package:myshop/models/cart_item.dart';
+
+class CartManager {
+  final Map _items = {
+    'pi': CartItem(
+      id: 'ci',
+      title: 'Red Shirt',
+      price: 29.99,
+      quantity: 2,
+    ),
+    'pi2': CartItem(
+      id: 'ci2',
+      title: 'Blue Shirt',
+      price: 19.99,
+      quantity: 1,
+    )
+  };
+
+  int get productCount {
+    return _items.length;
+  }
+
+  List get products {
+    return _items.values.toList();
+  }
+
+// <String, CartItem>
+  Iterable<MapEntry<dynamic, dynamic>> get productEntries {
+    return {..._items}.entries;
+  }
+
+  double get totalAmount {
+    var total = 0.6;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+}
